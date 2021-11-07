3e77094a3af48324054a153fe2dce752eaffd94a
Michal Cizmazia <440490+cizmazia@users.noreply.github.com>
2021-11-07 07:22:42 +0100

    fix lint errors

--- kdbx_cli.py
+++ kdbx_cli.py
@@ -1,6 +1,5 @@
 #! python3
 import pykeepass
-from pykeepass import icons
 from getpass import getpass
 from argparse import ArgumentParser
 from cmd import Cmd
@@ -12,6 +11,7 @@ from threading import Timer
 from copy import deepcopy
 import re
 import uuid
+
 
 class KeeCmd(Cmd):
     def __init__(self, kp):
@@ -35,7 +35,7 @@ class KeeCmd(Cmd):
 
     def do_s(self, arg):
         's [TITLE] # select entry by title'
-        entry = self.kp.find_entries(title=arg,first=True)
+        entry = self.kp.find_entries(title=arg, first=True)
         self.entry = entry
         self.prompt = prompt(entry)
 
@@ -60,11 +60,11 @@ class KeeCmd(Cmd):
 
     def do_dump(self, arg):
         'dump # display fields'
-        print('\n'.join('\033[36m' + k + '\033[0m: ' + v for k,v in fields(self.entry) if v))
+        print('\n'.join('\033[36m' + k + '\033[0m: ' + v for k, v in fields(self.entry) if v))
 
     def do_history(self, arg):
         'history # display changes'
-        print('\n'.join('\033[36m' + k + '\033[0m: ' + v for k,v in history(self.entry)))
+        print('\n'.join('\033[36m' + k + '\033[0m: ' + v for k, v in history(self.entry)))
 
     def do_p(self, arg):
         'p # clip password'
@@ -80,7 +80,7 @@ class KeeCmd(Cmd):
         stdin(ssh_add_fmt.format(**p), field(self.entry, 'password'))
 
     def complete_ssh(self, text, line, begin, end):
-        return [x for x in ['10s','10m','10h','10d'] if x.startswith(text)]
+        return [x for x in ['10s', '10m', '10h', '10d'] if x.startswith(text)]
 
     def do_sshknown(self, arg):
         'sshknown # add to known_hosts (from URL)'
@@ -153,7 +153,7 @@ class KeeCmd(Cmd):
     def do_add(self, arg):
         'add TITLE # new entry with title'
         e = None
-        if not self.kp.find_entries(title=arg,first=True):
+        if not self.kp.find_entries(title=arg, first=True):
             e = pykeepass.entry.Entry(arg, '', '', kp=self.kp)
             self.kp.root_group.append(e)
             self.save()
@@ -162,8 +162,7 @@ class KeeCmd(Cmd):
 
     def do_rename(self, arg):
         'rename TITLE # change title'
-        e = None
-        if not self.kp.find_entries(title=arg,first=True):
+        if not self.kp.find_entries(title=arg, first=True):
             self.update(lambda e: setattr(e, 'title', arg))
             self.prompt = prompt(self.entry)
 
@@ -217,26 +216,26 @@ class KeeCmd(Cmd):
 
     def do_aput(self, arg):
         'aput LABEL # change attribute by label'
-        l = arg.strip()
-        self.update(lambda e: e.set_custom_property(l, getpass(prompt='Attribute: ')))
+        label = arg.strip()
+        self.update(lambda e: e.set_custom_property(label, getpass(prompt='Attribute: ')))
 
     def complete_aput(self, text, line, begin, end):
         return attr_labels(self.entry, text)
 
     def do_apaste(self, arg):
         'apaste LABEL # paste new attribute by label'
-        l = arg.strip()
+        label = arg.strip()
         s = paste()
-        if l and s:
-            self.update(lambda e: e.set_custom_property(l, s))
+        if label and s:
+            self.update(lambda e: e.set_custom_property(label, s))
 
     def complete_apaste(self, text, line, begin, end):
         return attr_labels(self.entry, text)
 
     def do_arm(self, arg):
         'arm LABEL # remove attribute by label'
-        l = arg.strip()
-        self.update(lambda e: e.delete_custom_property(l))
+        label = arg.strip()
+        self.update(lambda e: e.delete_custom_property(label))
 
     def complete_arm(self, text, line, begin, end):
         return attr_labels(self.entry, text)
@@ -363,22 +362,22 @@ class KeeCmd(Cmd):
         export(a[0], e)
 
     def complete_cp(self, text, line, begin, end):
-        l = line.split()
-        if len(l) <= 1:
+        args = line.split()
+        if len(args) <= 1:
             return [f for f in listdir('.')]
-        elif len(l) == 2 and not line[-1:].isspace():
-            p, n = path.split(path.expanduser(l[1]))
+        elif len(args) == 2 and not line[-1:].isspace():
+            p, n = path.split(path.expanduser(args[1]))
             return [f for f in listdir(p or '.') if f.startswith(n)]
         else:
             return titles(self.kp, text)
 
     def do_mv(self, arg):
         'mv FILE ENTRY... # copy entries to db file, then move to recycle bin'
-        a = arg.split() or ['']
-        l = entries(self.kp, a[1:])
-        if export(a[0], l):
-            for e in l:
-                self.kp.trash_entry(e)
+        args = arg.split() or ['']
+        e = entries(self.kp, args[1:])
+        if export(args[0], e):
+            for entry in e:
+                self.kp.trash_entry(entry)
             self.save()
 
     def complete_mv(self, text, line, begin, end):
@@ -387,14 +386,14 @@ class KeeCmd(Cmd):
     def do_dedup(self, arg):
         'dedup # number duplicate titles'
         c = Counter(x.title for x in self.kp.entries)
-        d = [t for t,x in c.items() if x > 1]
+        d = [t for t, x in c.items() if x > 1]
         for t in d:
             dedup(self.kp, t)
         self.save()
 
     def do_passwd(self, arg):
         'passwd # change db password'
-        p =  getpass()
+        p = getpass()
         if p != getpass():
             print(None)
             return
@@ -412,6 +411,7 @@ class KeeCmd(Cmd):
         self.kp.save()
         self.mtime = stat(self.kp.filename).st_mtime
 
+
 def prompt(entry=None):
     p = '\n> '
     if not entry:
@@ -422,6 +422,7 @@ def prompt(entry=None):
     p = '\n\033[34m' + t + '\033[0m' + p
     return p
 
+
 def contains(e, arg):
     s = arg.lower()
     return (
@@ -431,6 +432,7 @@ def contains(e, arg):
         or s in ' '.join(note_labels(e)).lower()
         or s in ' '.join(attr_labels(e)).lower()
     )
+
 
 def fields(entry):
     return [
@@ -445,6 +447,7 @@ def fields(entry):
         ('History', str(len(entry.history)) if entry.history else ''),
     ] if entry else []
 
+
 def history(entry):
     e = parent_entry(entry)
     if not e:
@@ -457,19 +460,29 @@ def history(entry):
         e = h
     return cl
 
+
 def changes(new, old):
-    l = []
-    if not new or not old: return l
-    if new.title != old.title: l.append('Title')
-    if new.url != old.url: l.append('URL')
-    if new.username != old.username: l.append('Username')
-    if new.password != old.password: l.append('Password')
-    if notes(new) != notes(old): l.append('Notes')
-    if new.custom_properties != old.custom_properties: l.append('Attributes')
-    return l
+    res = []
+    if not new or not old:
+        return res
+    if new.title != old.title:
+        res.append('Title')
+    if new.url != old.url:
+        res.append('URL')
+    if new.username != old.username:
+        res.append('Username')
+    if new.password != old.password:
+        res.append('Password')
+    if notes(new) != notes(old):
+        res.append('Notes')
+    if new.custom_properties != old.custom_properties:
+        res.append('Attributes')
+    return res
+
 
 def field(entry, field):
     return getattr(entry, field, None)
+
 
 def notes(entry):
     return dict(
@@ -477,33 +490,41 @@ def notes(entry):
         for x in entry.notes.splitlines() if ':' in x
     ) if entry and entry.notes else {}
 
+
 def notes_str(d):
     return '\n'.join(': '.join(x) for x in d.items())
 
+
 def note_labels(entry, prefix=''):
-    return [x for x,_ in notes(entry).items() if x.startswith(prefix)]
+    return [x for x, _ in notes(entry).items() if x.startswith(prefix)]
+
 
 def note(entry, label):
     n = notes(entry)
     return n[label] if label in n else None
+
 
 def attr_labels(entry, prefix=''):
     return [
         x for x in entry.custom_properties.keys() if x.startswith(prefix)
     ] if entry else []
 
+
 def attr(entry, label):
     return entry.custom_properties[label] if entry and label in entry.custom_properties else None
 
+
 def entries(kp, titles, required=True):
-    l = [e for e in kp.entries if e.title in titles]
-    return l if not required or len(titles) == len(l) else None
+    res = [e for e in kp.entries if e.title in titles]
+    return res if not required or len(titles) == len(res) else None
+
 
 def titles(kp, prefix='', recycled=False):
     return [
         x.title for x in kp.entries
         if x.title.startswith(prefix) and recycled == in_recycle_bin(kp, x)
     ]
+
 
 def update(entry, f):
     if not entry or entry.is_a_history_entry:
@@ -512,6 +533,7 @@ def update(entry, f):
     entry.save_history()
     f(entry)
     entry.mtime = datetime.now()
+
 
 def dedup(kp, title):
     d = sorted(kp.find_entries(title=title), reverse=True, key=lambda x: x.mtime)
@@ -522,12 +544,14 @@ def dedup(kp, title):
         t = number(kp, entry.title)
         update(entry, lambda e: setattr(e, 'title', t))
 
+
 def number(kp, title):
     for i in range(len(kp.entries)):
         t = title + str(i)
-        if not kp.find_entries(title=t,first=True):
+        if not kp.find_entries(title=t, first=True):
             return t
     return None
+
 
 def parent_entry(entry):
     if not entry:
@@ -535,6 +559,7 @@ def parent_entry(entry):
     if not entry.is_a_history_entry:
         return entry
     return pykeepass.entry.Entry(element=entry._element.getparent().getparent(), kp=entry._kp)
+
 
 def clone(kp, entry, history=True):
     e = deepcopy(entry._element)
@@ -544,6 +569,7 @@ def clone(kp, entry, history=True):
     entry = pykeepass.entry.Entry(element=e, kp=kp)
     entry.uuid = uuid.uuid1()
     return entry
+
 
 def export(filename, entries):
     if not entries:
@@ -558,6 +584,7 @@ def export(filename, entries):
     kp.save()
     return True
 
+
 def in_recycle_bin(kp, entry):
     b = kp.recyclebin_group
     if not b:
@@ -570,34 +597,43 @@ def in_recycle_bin(kp, entry):
         g = g.parentgroup
     return False
 
+
 def to_int(s):
-    try: return int(s)
-    except ValueError: return None
+    try:
+        return int(s)
+    except ValueError:
+        return None
+
 
 def to_time_str(d):
     return d.astimezone().strftime('%Y.%m.%d_%H.%M.%S')
 
+
 # https://owasp.org/www-community/password-special-characters
 def owasp(b):
-    return alnum(b) or chr(b) in '''!"#$%&'()*+,-./:<=>?@[\]^_{|}'''
+    return alnum(b) or chr(b) in ('!"#$%&()*+,-./:<=>?@[]^_{|}' + "'" + "\\")
+
 
 def alnum(b):
     c = chr(b)
     return ('0' <= c and c <= '9') or ('A' <= c and c <= 'Z') or ('a' <= c and c <= 'z')
 
+
 def decimal(b):
     c = chr(b)
     return ('0' <= c and c <= '9')
 
+
 def filter_urandom(size, cond, block_size=512):
     if not size or size <= 0:
         return None
-    l = []
-    while len(l) < size:
+    res = []
+    while len(res) < size:
         for b in urandom(block_size):
             if cond(b):
-                l.append(b)
-    return bytes(l[:size])
+                res.append(b)
+    return bytes(res[:size])
+
 
 def open(filename):
     db = path.expanduser(filename)
@@ -609,6 +645,7 @@ def open(filename):
         print('wrong password')
         return None
 
+
 clip_cmd = 'xclip -sel c'
 paste_cmd = 'xclip -o -sel c'
 ssh_add_fmt = 'ssh-add -t {time} -'
@@ -617,29 +654,36 @@ ssh_known_cmd = 'sshknown'
 ssh_re = re.compile(r'(-*BEGIN[^\n]*.*\n-*END[^\n]*\n)(.*)', re.DOTALL)
 clip_seconds = 10
 
+
 def stdin(cmd, s):
     if not isinstance(s, str):
         print(None)
         return
     subprocess.run(cmd.split(), input=s.encode())
 
+
 def stdout(cmd):
     r = subprocess.run(cmd.split(), stdout=subprocess.PIPE)
     return r.stdout.decode() if r.returncode == 0 and r.stdout else None
 
+
 def clip(s):
     stdin(clip_cmd, s)
+
 
 def paste():
     return stdout(paste_cmd)
 
+
 def temp_clip(s):
     clip(s)
-    Timer(clip_seconds, lambda:paste()!=s or clip('')).start()
+    Timer(clip_seconds, lambda: paste() != s or clip('')).start()
+
 
 def ssh_gen():
     r = stdout(ssh_gen_cmd)
     return ssh_re.match(r).group(1, 2) if r else (None, None)
+
 
 def main(db):
     kp = open(db)
@@ -651,7 +695,8 @@ def main(db):
             break
         except (KeyboardInterrupt, FileExistsError) as e:
             print(type(e).__name__)
-            kp.read(password=kp.password,keyfile=kp.keyfile,transformed_key=kp.transformed_key)
+            kp.read(password=kp.password, keyfile=kp.keyfile, transformed_key=kp.transformed_key)
+
 
 if __name__ == '__main__':
     print('pykeepass ' + pykeepass.__version__)
